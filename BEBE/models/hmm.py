2dd3ab057ba1ef12d4e7076c78d1df4b240f8698
mcusi <maddie@earthspecies.org>
2023-03-16 01:11:36 +0000

    dmhmm->hmm

--- BEBE/models/hmm.py
+++ BEBE/models/hmm.py
@@ -1,104 +1,118 @@
-from BEBE.applications.ssm import ssm
-import yaml
 import numpy as np
+import jax.random as jr
+import jax.numpy as jnp
+import pandas as pd
 import pickle
 import os
 from matplotlib import pyplot as plt
+
+from dynamax.hidden_markov_model import GaussianHMM, DiagonalGaussianHMM
+
+from BEBE.models.preprocess import whitener_standalone, static_acc_filter, load_wavelet_transformed_data
 from BEBE.models.model_superclass import BehaviorModel
+
 
 class hmm(BehaviorModel):
   def __init__(self, config):
     super(hmm, self).__init__(config)
-    self.time_bins = self.model_config['time_bins']
-    
-  def fit(self):
-    train_data = []
-    train_fps = self.config['train_data_fp']
-    for fp in train_fps:
-      obs = self.load_model_inputs(fp)
-      
-      # Possibly subselect from training data:
-      if self.model_config['subselect_proportion'] < 1.:
-        total_len = np.shape(obs)[0]
-        to_select = int(np.ceil(total_len * self.model_config['subselect_proportion']))
-        start = int(rng.integers(0, high=total_len - to_select))
-        obs = obs[start:start + to_select, ...]
-        
-      obs_list = [obs[i* self.time_bins: (i+1)* self.time_bins, :] for i in range(len(obs)//self.time_bins)]
-      train_data.extend(obs_list)
-      
-    self.obs_dim = np.shape(train_data[0])[1]
-    
-    ###
-    # Compute transition matrix prior probabilities
-    
-    prior_wait_sec = self.model_config['prior_wait_sec']
-    prior_wait_samples = int(self.metadata['sr'] * prior_wait_sec)
-    prior_diagonal_entry = float(prior_wait_samples) / (1. + prior_wait_samples) # prior probability P_ii
-    
-    # Compute kappa and alpha based on prior diagonal entry
-    
-    prior_kappa_unscaled = (self.config['num_clusters'] * prior_diagonal_entry - 1.) / (self.config['num_clusters'] -1 )
-    prior_alpha_unscaled = (1. - prior_kappa_unscaled) / self.config['num_clusters']
-    
-    assert np.isclose(prior_kappa_unscaled + self.config['num_clusters'] * prior_alpha_unscaled, 1.)
-    
-    # Because of how ssm handles sticky transitions, 
-    # we have to specify how flat is the prior distribution over P_ii
-    
-    prior_strength = self.model_config['sticky_prior_strength'] # 0 is non-informative prior, 1. is as if we already have num_transitions_train_seen-many samples of evidence for the prior 
-    num_transitions_train_seen = len(train_data) * (self.time_bins-1)
-    
-    prior_scaling_factor = prior_strength * num_transitions_train_seen
-    
-    prior_kappa = prior_kappa_unscaled * prior_scaling_factor
-    prior_alpha = prior_alpha_unscaled * prior_scaling_factor + 1. # account for shift by 1. in sticky transition
-    
-    ###
-    # Instantiate HMM model
-    
-    if self.model_config['lags'] > 0:
-      self.model = ssm.HMM(self.config['num_clusters'],
-                           self.obs_dim,
-                           observations= "no_input_ar",
-                           observation_kwargs = {"lags" : self.model_config['lags']},
-                           transitions = "sticky", 
-                           transition_kwargs = {"alpha" : prior_alpha, "kappa" : prior_kappa}, 
+    self.random_seed = self.config['seed']
+    self.oom_limit = 10000
+
+    ## Model setup
+    if self.model_config["covariance"] == "full":
+      self.model_creation = lambda emission_dim: GaussianHMM(
+          self.config['num_clusters'], 
+          emission_dim, 
+          initial_probs_concentration=1.1, 
+          transition_matrix_concentration=self.model_config["matrix_concentration"],
+          transition_matrix_stickiness=0.0, 
+          emission_prior_mean=0.0, 
+          emission_prior_concentration=self.model_config["mean_concentration"], 
+          emission_prior_scale=self.model_config["cov_scale"],
+          emission_prior_extra_df=self.model_config["cov_df"]
+        )
+    elif self.model_config["covariance"] == "diagonal":
+      self.model_creation = lambda emission_dim: DiagonalGaussianHMM(
+                          self.config['num_clusters'], 
+                          emission_dim, 
+                          initial_probs_concentration=1.1, 
+                          transition_matrix_concentration=self.model_config["matrix_concentration"], 
+                          transition_matrix_stickiness=0.0, 
+                          emission_prior_mean=0.0, 
+                          emission_prior_mean_concentration=self.model_config["mean_concentration"], 
+                          emission_prior_concentration=self.model_config["cov_concentration"], 
+                          emission_prior_scale=self.model_config["cov_scale"]
                           )
+
+    ## Pre-processing setup
+    self.whiten = self.model_config['whiten']
+    self.n_components = self.model_config['n_components']
+    self.whitener = whitener_standalone(n_components = self.n_components)
+    self.wavelet_transform = self.model_config['wavelet_transform']
+    # wavelet transform: specific settings
+    self.morlet_w = self.model_config['morlet_w']
+    self.n_wavelets = self.model_config['n_wavelets']
+    self.downsample = self.model_config['downsample']
+
+  def load_model_inputs(self, filepath, train=False):
+    if not self.wavelet_transform:
+      data = pd.read_csv(filepath, delimiter = ',', header = None).values[:, self.cols_included]
+      data = static_acc_filter(data, self.config)
     else:
-      self.model = ssm.HMM(self.config['num_clusters'],
-                           self.obs_dim,
-                           observations= "gaussian",
-                           transitions = "sticky", 
-                           transition_kwargs = {"alpha" : prior_alpha, "kappa" : prior_kappa}, 
-                          )
-      
-    N_iters = self.model_config['N_iters']
-    hmm_lls = self.model.fit(train_data, 
-                             method= "em", 
-                             num_iters = N_iters, 
-                             init_method="kmeans",
-                            )
-    
+      data = load_wavelet_transformed_data(self, filepath, downsample = self.downsample)
+    if train: #for batching purposes
+      data = data[:-(data.shape[0] % self.model_config["time_bins"]), :]
+    return data
+
+  def fit(self):
+
+    train_data = []
+    for fp in self.config["train_data_fp"]:
+        x = self.load_model_inputs(fp, train=True)
+        x_batches = np.stack(np.split(x, int(x.shape[0]/self.model_config["time_bins"]), axis=0))
+        train_data.append(x_batches)
+
+    train_data = np.concatenate(train_data, axis=0)
+    n_batches, self.obs_dim, self.feature_dim = train_data.shape
+
+    if self.whiten:
+      train_data = self.whitener.fit_transform(train_data.reshape(-1,self.feature_dim)).reshape(n_batches,self.obs_dim,-1)
+      self.feature_dim = train_data.shape[2]
+
+    ###
+    key = jr.PRNGKey(self.config['seed'])
+    train_data = jnp.array(train_data)
+    self.model = self.model_creation(self.feature_dim)
+    params, param_props = self.model.initialize(key=key, method="kmeans", emissions=train_data)
+    self.model_params, log_probs = self.model.fit_em(params, param_props, train_data, num_iters=self.model_config["N_iters"])
+    bad_optimization = np.any(np.isnan(log_probs))
+
     # Save log likelihood and transition plots
-    plt.plot(hmm_lls, label="EM")
-    plt.title("Train log probability")
+    plt.plot(log_probs, label="EM")
+    plt.title(f"Train log probability, trained to completion: {not bad_optimization}")
     plt.xlabel("EM Iteration")
     plt.ylabel("Log Probability")
     lls_fp = os.path.join(self.config['visualization_dir'], 'train_lls.png')
     plt.savefig(lls_fp)
     plt.close()
-    
+
+    if bad_optimization:
+      raise Exception(f"HMM did not optimize for all {self.model_config['N_iters']} iterations, ran into NaNs.")
+
     trans_fp = os.path.join(self.config['visualization_dir'], 'trans_probs.png')
-    plt.imshow(self.model.transitions.transition_matrix)
+    plt.imshow(self.model_params.transitions.transition_matrix)
     plt.savefig(trans_fp)
     plt.close()
     
   def save(self):
+    self.model_creation = None #Pickle can't save local lambda, and we don't need it anymore
     target_fp = os.path.join(self.config['final_model_dir'], "final_model.pickle")
     with open(target_fp, 'wb') as f:
       pickle.dump(self, f)
   
   def predict(self, data):
-    predictions = self.model.most_likely_states(data)
+    # assert len(data.shape) == 2 #time, features
+    if self.whiten:
+      data = self.whitener.transform(data)
+    predictions = self.model.most_likely_states(self.model_params, data)
     return predictions, None
