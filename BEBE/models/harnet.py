4371f3cbbd38e71dbf688c71e8412354678936a8
Benjamin Hoffman <benjaminsshoffman@gmail.com>
2023-09-17 13:33:06 +0000

    add harnet support

---
+++ BEBE/models/harnet.py
@@ -0,0 +1,66 @@
+import yaml
+import numpy as np
+import torch
+import torch.nn as nn
+from BEBE.models.supervised_nn_utils import SupervisedBehaviorModel
+from torch_intermediate_layer_getter import IntermediateLayerGetter as MidGetter
+
+class harnet(SupervisedBehaviorModel):
+  def __init__(self, config):
+    super(harnet, self).__init__(config)
+    
+    self.unknown_label = config['metadata']['label_names'].index('unknown')
+    self.harnet_version = self.model_config['harnet_version']  
+  
+    self.model = Classifier(self.n_features,
+                            self.n_classes,
+                            self.device,
+                            self.harnet_version).to(self.device)
+      
+    print(self.model)
+
+class Classifier(nn.Module):
+    def __init__(self, n_features, n_classes, device, harnet_version):
+        super(Classifier, self).__init__()
+        
+        harnet = torch.hub.load('OxWearables/ssl-wearables', harnet_version, class_num=5, pretrained=True).to(device).eval()
+        
+        return_layers = {
+            'feature_extractor.layer1': 'layer1',
+            'feature_extractor.layer2': 'layer2',
+            'feature_extractor.layer3': 'layer3',
+        }
+        self.conv = MidGetter(harnet, return_layers=return_layers, keep_output=True)
+        
+        self.head = nn.Linear(128, n_classes).to(device)
+        
+        n_channels_fusion = 128 + n_features-3
+        self.gru = nn.GRU(n_channels_fusion, 64, num_layers = 1, bidirectional = True, batch_first = True)
+        
+        
+    def forward(self, x):
+        # X is [batch, seq_len, channels]
+        
+        seq_len = x.size()[-2]
+        n_channels = x.size()[-1]
+        
+        if n_channels>3:
+          x_extra = x[:,:,3:]
+          x = x[:,:,:3]
+        
+        with torch.no_grad():
+          x = torch.transpose(x, -1, -2)
+          mids, _ = self.conv(x)
+          x = mids['layer2']
+          x = torch.nn.functional.interpolate(x, size = seq_len)
+          x = torch.transpose(x, -1, -2)
+          if n_channels>3:
+            x = torch.cat([x, x_extra], dim = -1)
+        
+        x, _ = self.gru(x)
+        x = self.head(x)
+        
+        x = torch.transpose(x, -1, -2)
+               
+        return x  
+
