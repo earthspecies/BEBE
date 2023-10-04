d7de4dfbdbf5173b30524a68a33543eb6e19f8ba
mcusi <maddie@earthspecies.org>
2023-10-04 18:43:38 +0000

    config for classic hyperparameters

--- BEBE/models/classic.py
+++ BEBE/models/classic.py
@@ -6,6 +6,8 @@ from sklearn.svm import LinearSVC
 
 from BEBE.models.supervised_classic_utils import ClassicBehaviorModel
 
+# TODO for all models here: should class_weight depend on dataset proportions?
+
 class RandomForest(ClassicBehaviorModel):
   def __init__(self, config):
     super().__init__(config)
@@ -22,16 +24,14 @@ class DecisionTree(ClassicBehaviorModel):
     super().__init__(config)
     self.n_samples_window = max(int(np.ceil(self.model_config['context_window_sec'] * self.metadata['sr'])), 3)
     self.min_samples_split = self.model_config['min_samples_split']
-    # TODO: Pull relevant parameters from self.model_config that we want to use in hyperparameter search
-    # TODO: should class_weight depend on dataset proportions?
-    self.model = DecisionTreeClassifier(max_depth=None, min_samples_split=self.min_samples_split, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=self.config['seed'], max_leaf_nodes=None, min_impurity_decrease=0.0, class_weight=None, ccp_alpha=0.0)
+    self.model = DecisionTreeClassifier(min_samples_split=self.min_samples_split, random_state=self.config['seed'], class_weight=None)
 
 class SupportVectorMachine(ClassicBehaviorModel):
   def __init__(self, config):
     super().__init__(config)
     self.n_samples_window = max(int(np.ceil(self.model_config['context_window_sec'] * self.metadata['sr'])), 3)
     self.min_samples_split = self.model_config['min_samples_split']
-    # TODO: Pull relevant parameters from self.model_config that we want to use in hyperparameter search
-    # TODO: should class_weight depend on dataset proportions?
-    # TODO: LinearSVC is recommended for large datasets
-    self.model = LinearSVC(penalty='l2', loss='squared_hinge', dual=False, tol=0.0001, C=1.0, multi_class='ovr', fit_intercept=True, intercept_scaling=1, class_weight=None, verbose=0, random_state=self.config['seed'], max_iter=1000)
+    self.max_iter = self.model_config['max_iter']
+    self.C = self.model_config['regularization_parameter']
+    self.intercept_scaling = self.model_config['intercept_scaling']
+    self.model = LinearSVC(dual=False, C=self.C, intercept_scaling=self.intercept_scaling, class_weight=None, verbose=2, random_state=self.config['seed'], max_iter=self.max_iter)
